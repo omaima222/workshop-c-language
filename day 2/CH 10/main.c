c79f8ac8c9249b86275f64766bae9fd2a81c997a
omaima <oumaimaelmachi@gmail.com>
2022-09-08 07:06:58 +0200

    newest

---
+++ day 2/CH 10/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float rayon;
+    printf("entrer le rayon : ");
+    scanf("%f", &rayon);
+    printf("la Circonference est : %f", 2 * 3.14159265358979323846 * rayon  );
+}
